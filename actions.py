8fcf436c0cb040c03baad2f65c5d922219e210b0
matte <matteofranzil@gmail.com>
2018-04-24 15:51:37 +0200

    Added support for Direttivo message

--- actions.py
+++ actions.py
@@ -3,33 +3,37 @@
 import logging as log
 
 from google_calendar import getAssignedPeople
-from secrets import groups, group_chat_id
+from secrets import groups, group_chat_id, direttivoid
+
+
+class ReplyStatus:
+    direttivoresponse = False
 
 
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id,
-                     text="Ciao! Questo è il bot del Cooking Corner del Nest, mantenuto dal club Tech@Nest."
+    bot.sendMessage(chat_id=update.message.chat_id,
+                    text="Ciao! Questo è il bot del Cooking Corner del Nest, mantenuto dal club Tech@Nest."
                           + ". Per iniziare scrivi un comando o scrivi /help per aiuto.\nOgni altra richiesta verrà ignorata.")
 
 
 def help(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="/oggi - Mostra il turno di oggi" +
-                                                          "\n/domani - Mostra il turno di domani" +
-                                                          "\n/gruppo NUMEROGRUPPO - Mostra i membri di un certo gruppo" +
-                                                          "\n/info - Ottieni informazioni sul bot")  # +
-    # "\n/direttivo - Ottieni informazioni sul Direttivo del Cooking Corner")
+    bot.sendMessage(chat_id=update.message.chat_id,
+                    text="/info - Ottieni informazioni sul e sul Direttivo del Cooking Corner" +
+                         "\n/oggi - Mostra il turno di oggi" +
+                         "\n/domani - Mostra il turno di domani" +
+                         "\n/gruppo <#> - Mostra i membri di un certo gruppo" +
+                         "\n/direttivo - Conttatta il Direttivo del Cooking Corner")
 
 
 def info(bot, update):
-    bot.send_message(chat_id=update.message.chat_id,
-                     text="Ciao! Questo bot è stato creato dal club Tech@Nest durante un Hackaton nel novembre 2017." +
+    bot.sendMessage(chat_id=update.message.chat_id,
+                    text="Ciao! Questo bot è stato creato dal club Tech@Nest durante un Hackaton nel novembre 2017." +
                           " Il bot è stato ideato da Gianvito Taneburgo, ora non più al Nest. Al momento il bot è mantenuto da Matteo Franzil" +
                           ", se serve aiuto conttattalo su @mfranzil.")
+    bot.send_message(chat_id=update.message.chat_id,
+                     text="Membri del Direttivo:\n\nSofia Caruso, Matteo Franzil, Matteo Marra, " +
+                          "Alice Massa, Francesco Misiano, Nicola Pozza, Giovanni Rachello. ")
 
-
-# TODO Finire il metodo direttivo
-# def direttivo(bot, update):
-#    bot.send_message(chat_id=update.message.chat_id, text="")
 
 def todayTurn(bot, update):
     assigned_group = getAssignedPeople(0)
@@ -41,7 +45,7 @@ def todayTurn(bot, update):
             bot.sendMessage(update.message.chat_id, message)
     except Exception as ex:
         log.error("Unable to send Telegram message!\n" + ex.message)
-        bot.sendMessage(update.message.chat_id, "Mi dispiace, in questo momento non riesco a ottenere i turni di oggi.")
+        bot.sendMessage(update.message.chat_id, "Nessun turno è in programma per oggi!")
 
 
 def tomorrowTurn(bot, update):
@@ -55,12 +59,7 @@ def tomorrowTurn(bot, update):
     except Exception as ex:
         log.error("Unable to send Telegram message!\n" + ex.message)
         bot.sendMessage(update.message.chat_id,
-                        "Mi dispiace, in questo momento non riesco a ottenere i turni di domani.")
-
-
-def defaultResponse(bot, update):
-    if (update.message.chat_id != group_chat_id):
-        bot.sendMessage(update.message.chat_id, "Mi dispiace, posso solo risponderti se usi uno dei comandi in /help.")
+                        "Nessun turno previsto per domani!")
 
 
 def getGroup(bot, update, args):
@@ -72,5 +71,35 @@ def getGroup(bot, update, args):
         bot.sendMessage(update.message.chat_id, message)
     except Exception as ex:
         bot.sendMessage(update.message.chat_id,
-                        "Non ho capito bene il numero del gruppo. I gruppi possibili vanno da 1 a 40.")
+                        "Scrivi /gruppo seguito da un numero da 1 a 40 per ottenere informazioni sul gruppo.")
         log.error(ex.message)
+
+
+def direttivo(bot, update):
+    chat_id = update.message.chat_id
+    bot.sendMessage(chat_id=chat_id,
+                    force_reply=True,
+                    text="Rispondi a questo messaggio per recapitare un messaggio " + \
+                         "al Direttivo. Segnalazioni, suggerimenti sono ben accetti. Eventuali abusi saranno puniti con un richiamo.")
+    ReplyStatus.direttivoresponse = True
+
+
+def risposteDirettivo(bot, update):
+    try:  # TODO Add datastore to failed message
+        if ReplyStatus.direttivoresponse:
+            if update.message.from_user.last_name == None:
+                bot.sendMessage(chat_id=direttivoid, text=str(update.message.from_user.first_name) + " scrive:\n\n" + \
+                                                          update.message.text)
+            else:
+                bot.sendMessage(chat_id=direttivoid, text=str(update.message.from_user.first_name) + " " + \
+                                                          str(update.message.from_user.last_name) + " scrive:\n\n" + \
+                                                          update.message.text)
+            bot.sendMessage(chat_id=update.message.chat_id, text="Messaggio inviato con successo.")
+    except Exception as ex:
+        log.error(ex.message)
+    ReplyStatus.direttivoresponse = False
+
+
+def default(bot, update):
+    if not (update.message.chat_id == group_chat_id):
+        bot.sendMessage(update.message.chat_id, "Mi dispiace, posso solo risponderti se usi uno dei comandi in /help.")
--- webhook.py
+++ webhook.py
@@ -4,7 +4,7 @@ import telegram
 import webapp2
 from telegram.ext import Dispatcher, CommandHandler
 
-from actions import start, todayTurn, getGroup, help, tomorrowTurn
+from actions import start, todayTurn, getGroup, help, tomorrowTurn, direttivo, info
 from secrets import url, ccn_bot_token
 
 ccn_bot = telegram.Bot(ccn_bot_token)
@@ -13,11 +13,11 @@ ccn_bot = telegram.Bot(ccn_bot_token)
 class WebHookHandler(webapp2.RequestHandler):
     def get(self):
         dispatcherSetup()
-        s = ccn_bot.setWebhook(url + ccn_bot_token)
-        if s:
-            self.response.write("Webhook setted")
+        res = ccn_bot.setWebhook(url + ccn_bot_token)
+        if res:
+            self.response.write("Webhook set up!")
         else:
-            self.response.write("Webhook setup failed")
+            self.response.write("Webhook setup failed...")
 
 
 class UpdateHandler(webapp2.RequestHandler):
@@ -33,9 +33,11 @@ def dispatcherSetup():
     global dispatcher
     dispatcher = Dispatcher(bot=ccn_bot, update_queue=None, workers=0)
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("oggi", todayTurn))
-    dispatcher.add_handler(CommandHandler("gruppo", getGroup, pass_args=True))
     dispatcher.add_handler(CommandHandler("help", help))
+    dispatcher.add_handler(CommandHandler("info", info))
+    dispatcher.add_handler(CommandHandler("oggi", todayTurn))
     dispatcher.add_handler(CommandHandler("domani", tomorrowTurn))
+    dispatcher.add_handler(CommandHandler("gruppo", getGroup, pass_args=True))
+    dispatcher.add_handler(CommandHandler("direttivo", direttivo))
     # dispatcher.add_handler(MessageHandler(Filters.text, defaultResponse))
     return dispatcher
